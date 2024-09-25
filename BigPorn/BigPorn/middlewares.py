976ca898b97cabc7ff72868822f3a49201b3db4a
Cameron Angeleri <cam.angeleri@gmail.com>
2024-09-25 12:49:44 -0700

    bringing in imports & int of spider V2

--- BigPorn/BigPorn/middlewares.py
+++ BigPorn/BigPorn/middlewares.py
@@ -7,21 +7,9 @@ from scrapy import signals
 
 # useful for handling different item types with a single interface
 from itemadapter import is_item, ItemAdapter
-from proxy_data import proxy_rotation
-import logging
 
 
-class ProxyMiddleware:
-    def process_request(self, request, spider):
-        try:
-            rotated_proxy = proxy_rotation()
-            request.meta["proxy"] = rotated_proxy
-        # logging.info(f"Proxy assigned: {rotated_proxy} for request: {request.url}")
-        except Exception as e:
-            logging.error(f"Error assigning proxy for {request.url}: {e}")
-
-
-class AdultdeepfakeSpiderMiddleware:
+class BigpornSpiderMiddleware:
     # Not all methods need to be defined. If a method is not defined,
     # scrapy acts as if the spider middleware does not modify the
     # passed objects.
@@ -68,7 +56,7 @@ class AdultdeepfakeSpiderMiddleware:
         spider.logger.info("Spider opened: %s" % spider.name)
 
 
-class AdultdeepfakeDownloaderMiddleware:
+class BigpornDownloaderMiddleware:
     # Not all methods need to be defined. If a method is not defined,
     # scrapy acts as if the downloader middleware does not modify the
     # passed objects.
--- BigPorn/BigPorn/settings.py
+++ BigPorn/BigPorn/settings.py
@@ -1,4 +1,4 @@
-# Scrapy settings for adultdeepfake project
+# Scrapy settings for BigPorn project
 #
 # For simplicity, this file contains only settings considered important or
 # commonly used. You can find more settings consulting the documentation:
@@ -7,93 +7,85 @@
 #     https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
 #     https://docs.scrapy.org/en/latest/topics/spider-middleware.html
 
-BOT_NAME = "adultdeepfake"
+BOT_NAME = "BigPorn"
 
-SPIDER_MODULES = ["adultdeepfake.spiders"]
-NEWSPIDER_MODULE = "adultdeepfake.spiders"
-
-
-from proxy_data import start_proxy_scheduler
-
-# Start the proxy scheduler
-start_proxy_scheduler()
+SPIDER_MODULES = ["BigPorn.spiders"]
+NEWSPIDER_MODULE = "BigPorn.spiders"
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-# USER_AGENT = "adultdeepfake (+http://www.yourdomain.com)"
+#USER_AGENT = "BigPorn (+http://www.yourdomain.com)"
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-# CONCURRENT_REQUESTS = 32
+#CONCURRENT_REQUESTS = 32
 
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-# DOWNLOAD_DELAY = 3
+#DOWNLOAD_DELAY = 3
 # The download delay setting will honor only one of:
-# CONCURRENT_REQUESTS_PER_DOMAIN = 16
-# CONCURRENT_REQUESTS_PER_IP = 16
+#CONCURRENT_REQUESTS_PER_DOMAIN = 16
+#CONCURRENT_REQUESTS_PER_IP = 16
 
 # Disable cookies (enabled by default)
-# COOKIES_ENABLED = False
+#COOKIES_ENABLED = False
 
 # Disable Telnet Console (enabled by default)
-# TELNETCONSOLE_ENABLED = False
+#TELNETCONSOLE_ENABLED = False
 
 # Override the default request headers:
-# DEFAULT_REQUEST_HEADERS = {
+#DEFAULT_REQUEST_HEADERS = {
 #    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
 #    "Accept-Language": "en",
-# }
+#}
 
 # Enable or disable spider middlewares
 # See https://docs.scrapy.org/en/latest/topics/spider-middleware.html
-# SPIDER_MIDDLEWARES = {
-#    "adultdeepfake.middlewares.adultdeepfakeSpiderMiddleware": 543,
-# }
+#SPIDER_MIDDLEWARES = {
+#    "BigPorn.middlewares.BigpornSpiderMiddleware": 543,
+#}
 
 # Enable or disable downloader middlewares
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
-DOWNLOADER_MIDDLEWARES = {
-    #    "adultdeepfake.middlewares.adultdeepfakeDownloaderMiddleware": 543,
-    "scrapy.downloadermiddlewares.httpproxy.HttpProxyMiddleware": 110,
-    "adultdeepfake.middlewares.ProxyMiddleware": 100,
-}
+#DOWNLOADER_MIDDLEWARES = {
+#    "BigPorn.middlewares.BigpornDownloaderMiddleware": 543,
+#}
 
 # Enable or disable extensions
 # See https://docs.scrapy.org/en/latest/topics/extensions.html
-# EXTENSIONS = {
+#EXTENSIONS = {
 #    "scrapy.extensions.telnet.TelnetConsole": None,
-# }
+#}
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-# ITEM_PIPELINES = {
-#    "adultdeepfake.pipelines.adultdeepfakePipeline": 300,
-# }
+#ITEM_PIPELINES = {
+#    "BigPorn.pipelines.BigpornPipeline": 300,
+#}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
-# AUTOTHROTTLE_ENABLED = True
+#AUTOTHROTTLE_ENABLED = True
 # The initial download delay
-# AUTOTHROTTLE_START_DELAY = 5
+#AUTOTHROTTLE_START_DELAY = 5
 # The maximum download delay to be set in case of high latencies
-# AUTOTHROTTLE_MAX_DELAY = 60
+#AUTOTHROTTLE_MAX_DELAY = 60
 # The average number of requests Scrapy should be sending in parallel to
 # each remote server
-# AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
+#AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
 # Enable showing throttling stats for every response received:
-# AUTOTHROTTLE_DEBUG = False
+#AUTOTHROTTLE_DEBUG = False
 
 # Enable and configure HTTP caching (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
-# HTTPCACHE_ENABLED = True
-# HTTPCACHE_EXPIRATION_SECS = 0
-# HTTPCACHE_DIR = "httpcache"
-# HTTPCACHE_IGNORE_HTTP_CODES = []
-# HTTPCACHE_STORAGE = "scrapy.extensions.httpcache.FilesystemCacheStorage"
+#HTTPCACHE_ENABLED = True
+#HTTPCACHE_EXPIRATION_SECS = 0
+#HTTPCACHE_DIR = "httpcache"
+#HTTPCACHE_IGNORE_HTTP_CODES = []
+#HTTPCACHE_STORAGE = "scrapy.extensions.httpcache.FilesystemCacheStorage"
 
 # Set settings whose default value is deprecated to a future-proof value
 REQUEST_FINGERPRINTER_IMPLEMENTATION = "2.7"
